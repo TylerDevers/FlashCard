4b1fa75f1b237ac3fbe6a326350128b0f02a452b
TylerDevers <devers.tyler@gmail.com>
2020-03-05 11:07:12 -0800

    seperating q and a files into categories. added gitignore

--- flash_card_fun.py
+++ flash_card_fun.py
@@ -1,22 +1,34 @@
+#! /usr/bin/python3
+
 import subprocess
+from os import listdir #get files from a directory
+import os
+
+def list_files_in_path(my_path):
+    return listdir(my_path)
+
 
 def user_chooses_questions():
-    choice = 0
-    file_count = 2
-
-    while not 1 <= choice <= file_count:
-        choice = input(" Enter the Number for the questions you like to review:\n1) questions that need better answers\n2)QandA\n")
+    choice = -1
+    my_file_list = list_files_in_path("questions_dir")
+    file_count = len(my_file_list)
+    
+    while not 0 <= choice < file_count:
+        print("Enter the option number to choose the category of questions you prefer:\n")
+        for item in my_file_list:
+            print(f"{my_file_list.index(item)})  {item}\n")
+        choice = input()
         try:
             choice = int(choice)
         except:
             subprocess.run("clear")
             print("That was not a number. Please enter a numeral.\n")
-            choice = 0
+            choice = -1
 
-        if not 1 <= choice <= file_count:
+        if not 0 <= choice <= file_count:
+            subprocess.run("clear")
             print("Remeber to enter a number from one of the choices.\n")
 
-    if choice == 1:
-        return("questions_to_research")
-    elif choice == 2:
-        return("QandA")
+    return(my_file_list[choice])
+   
+
--- main.py
+++ main.py
@@ -3,22 +3,26 @@
 # TODO sort out user_to_choose function. Issues with file name being opened by the reader.
 import csv
 import subprocess # needed to run $ clear bash command.
-from flash_card_fun import *
+from flash_card_fun import user_chooses_questions
 
 QandA_dict = {}
 questions_completed_dict = {}
-file_to_use = user_chooses_questions()
 
+# gets input from user about which file to read questions from.
+file_to_use = f"questions_dir/{user_chooses_questions()}"
+
+# Reads questions file and stores in Dictionary for easy iteration
 with open(file_to_use, mode="r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     for row in csv_reader:
         # print(row) # for debugging
         QandA_dict.update( {row[0]:row[1]} )
 
-
-# for subject in QandA_dict:
-#     print(f"{subject}\n")
-#     input("Press enter to continue.\n")
-#     print(f"{QandA_dict[subject]} \n")
-#     input("Press enter to continue.\n")
-#     subprocess.run('clear')
+# Prints question to standard output
+for question in QandA_dict:
+    answer = QandA_dict[question]
+    print(f"{question}\n")
+    input("Press enter to continue.\n")
+    print(f"{answer} \n")
+    input("Press enter to continue.\n")
+    subprocess.run('clear')
